3aee7dc6352e9b791d2985dca57afd14cdca4de9
I0re <ioresilvarock@gmail.com>
2022-09-27 11:25:31 -0300

    first commit

---
+++ chj/orders.js
@@ -0,0 +1,32 @@
+const Orders = [
+    {
+        productName: 'foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'foldable Mini Drone',
+        productNumber: '36378',
+        paymentStatus: 'Due',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'foldable Mini Drone',
+        productNumber: '493447',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'foldable Mini Drone',
+        productNumber: '96996',
+        paymentStatus: 'Due',
+        shipping: 'Deliverd'
+    },
+    {
+        productName: 'foldable Mini Drone',
+        productNumber: '22821',
+        paymentStatus: 'Due',
+        shipping: 'Deliverd'
+    },
+]
